a85cf4eebb25364ea5ed354f3e300e3d7e4fcf47
sgoparapu <34513908+sgoparapu@users.noreply.github.com>
2020-02-23 14:20:34 +0530

    updated vaules

--- variables.tf
+++ variables.tf
@@ -1,10 +1,10 @@
 ### This ideally has the Secure keys so that we can establish the connection to AWS Please change accordinlgy
 
 variable "aws_access_key" {
-  default = "AKIA3FL6J7SZITQASLUG"
+  default = ""
 }
 variable "aws_secret_key" {
-  default = "DhjPKKN19Rb8Dt4sevhUVZ0PYK0xEK2st12SSmUu"
+  default = ""
 }
 variable "aws_region" {
   default = "us-west-2"
