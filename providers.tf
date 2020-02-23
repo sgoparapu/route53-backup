60be469a35fbac883ea39fe0c22eab0bb77a3007
sgoparapu <34513908+sgoparapu@users.noreply.github.com>
2020-02-23 15:32:52 +0530

    modified for multiple accounts

--- providers.tf
+++ providers.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  access_key = "${var.aws_access_key}"
-  secret_key = "${var.aws_secret_key}"
-  region     = "${var.aws_region}"
+  region                  = "us-west-2"
+  shared_credentials_file = "/home/sidharthg/.aws/credentials"
+  profile                 = "default"
 }
